49480fbd3670ac4c435fa9445111072e7f70210c
Capybara77 <andrei7318@yandex.ru>
2024-11-02 00:56:38 +0500

    прием смс. начало

---
+++ backend-asp-net/luna2000/Controllers/SmsController.cs
@@ -0,0 +1,36 @@
+﻿using luna2000.Data;
+using luna2000.Dto;
+using luna2000.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace luna2000.Controllers;
+
+public class SmsController : ControllerBase
+{
+    private readonly LunaDbContext _dbContext;
+
+    public SmsController(LunaDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpPost]
+    public IActionResult Receive(SmsReceiveRequest smsReceiveRequest)
+    {
+        var message = $"New message receive from {smsReceiveRequest.Sender} with text {smsReceiveRequest.Message}";
+        Console.WriteLine(message);
+
+        _dbContext.Set<BaseLog>()
+            .Add(new BaseLog
+            {
+                ChangeId = Guid.NewGuid(),
+                Created = DateTime.UtcNow,
+                EventType = EventType.Add,
+                Note = message
+            });
+
+        _dbContext.SaveChanges();
+
+        return Ok();
+    }
+}
---
+++ backend-asp-net/luna2000/Dto/SmsReceiveRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace luna2000.Dto;
+
+public class SmsReceiveRequest
+{
+    public string Sender { get; set; }
+
+    public string Message { get; set; }
+}
